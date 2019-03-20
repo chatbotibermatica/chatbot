f1d90058300089e5cde935f447b062913421ccc7
Garcia Alcalde <MAGAALFE@intranet.ibermatica>
2019-03-20 16:05:27 +0100

    c

--- API/Controllers/GameDataController.js
+++ API/Controllers/GameDataController.js
@@ -17,22 +17,26 @@ function getTeamInfo(req, res) {
         if (err) {
             return res.json({
                 speech: 'Something went wrong!',
-                fulfillmentText: 'Something went wrong!',
+                displayText: 'Something went wrong!',
                 source: 'team info'
             });
         }
         if (teamExists) {
             return res.json({
-                queryResult: {
-                    speech: teamExists.description,
-                    fulfillmentText: teamExists.description,
-                    source: 'team info'
-                }
+                "fulfillmentMessages": [{
+                    "platform": "ACTIONS_ON_GOOGLE",
+                    "simpleResponses": {
+                        "simpleResponses": [{
+                            "displayText": "Sorry, something went wrong",
+                            "ssml": "<speak>Sorry, Something went wrong <break time=\"200ms\"/> Please try again later</speak>"
+                        }]
+                    }
+                }]
             });
         } else {
             return res.json({
                 speech: 'Currently I am not having information about this team',
-                fulfillmentText: 'Currently I am not having information about this team',
+                displayText: 'Currently I am not having information about this team',
                 source: 'team info'
             });
         }
@@ -50,7 +54,7 @@ function getTeamSchedule(req, res) {
                 if (err) {
                     return res.json({
                         speech: 'Something went wrong!',
-                        fulfillmentText: 'Something went wrong!',
+                        displayText: 'Something went wrong!',
                         source: 'game schedule'
                     });
                 }
@@ -66,14 +70,14 @@ function getTeamSchedule(req, res) {
                             }
                             return res.json({
                                 speech: 'Last game between Kings and ' + parameters.team + ' was played on ' + game.date + ' .' + winningStatement,
-                                fulfillmentText: 'Last game between Kings and ' + parameters.team + ' was played on ' + game.date + ' .' + winningStatement,
+                                displayText: 'Last game between Kings and ' + parameters.team + ' was played on ' + game.date + ' .' + winningStatement,
                                 source: 'game schedule'
                             });
                             //break;
                         } else {
                             return res.json({
                                 speech: 'Cant find any previous game played between Kings and ' + parameters.team,
-                                fulfillmentText: 'Cant find any previous game played between Kings and ' + parameters.team,
+                                displayText: 'Cant find any previous game played between Kings and ' + parameters.team,
                                 source: 'game schedule'
                             });
                         }
@@ -84,14 +88,14 @@ function getTeamSchedule(req, res) {
         } else {
             return res.json({
                 speech: 'Next game schedules will be available soon',
-                fulfillmentText: 'Next game schedules will be available soon',
+                displayText: 'Next game schedules will be available soon',
                 source: 'game schedule'
             });
         }
     } else {
         return res.json({
             speech: 'Cant handle the queries with two teams now. I will update myself',
-            fulfillmentText: 'Cant handle the queries with two teams now. I will update myself',
+            displayText: 'Cant handle the queries with two teams now. I will update myself',
             source: 'game schedule'
         });
     }
